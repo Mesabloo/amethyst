3a57d40cf3638095aff1447e6063bb485aebcc55
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2019-10-08 10:50:40 +0200

    Parsing tests

--- app/Main.hs
+++ app/Main.hs
@@ -8,7 +8,7 @@ import qualified Text.Megaparsec as Mega
 import Control.Monad
 
 main :: IO ()
-main = case Mega.runParser program "test" "\\f { \\a 3 = } = f a" of
+main = case Mega.runParser program "test" "\\test {{2 +} swap {3 +} swap ?:} = 0 1 test" of
     Left e -> putStrLn (Mega.errorBundlePretty e)
     Right p ->
         runEval (eval p) >>= \(s, x) -> print s *> case x of
